7d5d92c2a7a5d72ab520fd06db8a2e1062daf130
Solar <A04595@ibfs.com.tw>
2025-12-23 10:58:46 +0800

    'stockinfo'

--- app/robot.js
+++ app/robot.js
@@ -5,7 +5,11 @@ export default function robots() {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: [
+        '/private/',            // 阻擋 private 資料夾
+        '/*?*openModal=true',   // 阻擋彈跳視窗參數
+        '/*?*date=',            // 🔥 新增：阻擋日期查詢參數
+      ],
     },
     sitemap: 'https://ugoodly.com/sitemap.xml',
   }
